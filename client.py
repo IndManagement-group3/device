b26ec973396665bad5c694aac8a295eb069e0882
Kirs67 <kirilel200267@gmail.com>
2021-05-20 23:21:25 +0300

    Actual PWM control added

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+pwmpin = 12
+pwmfreq = 1000
+
+devid = "fan"
 
 turned = 0
 power = 0
+
+GPIO.setmode(GPIO.BOARD) #pins are numbered according to their physical order on the header
+GPIO.setup(pwmpin, GPIO.OUT)
+pwm = GPIO.PWM(pwmpin, pwmfreq)
+pwm.start(0)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -10,9 +21,9 @@ def on_connect(client, userdata, flags, rc):
 
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	client.subscribe("/fan/turn")
-	client.subscribe("/fan/power")
-	client.subscribe("/fan/update")
+	client.subscribe("/{0}/turn".format(devid))
+	client.subscribe("/{0}/power".format(devid))
+	client.subscribe("/{0}/update".format(devid))
 
 	send_status(client)
 
@@ -20,7 +31,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 	global turned, power
 
-	if msg.topic == "/fan/turn":
+	if msg.topic == "/{0}/turn".format(devid):
 		turned = int(msg.payload.decode("utf-8"))
 
 		if turned == 1:
@@ -31,10 +42,12 @@ def on_message(client, userdata, msg):
 		print("Fan is turned", state)
 
 
-	elif msg.topic == "/fan/power":
+	elif msg.topic == "/{0}/power".format(devid):
 		power = int(msg.payload.decode("utf-8"))
 
 		print("Power is {0}%".format(power))
+
+	pwm.ChangeDutyCycle(power * turned)
 
 	send_status(client)
 
@@ -43,7 +56,7 @@ def send_status(client):
 
 	status = "{0};{1}".format(turned, power)
 	print(status)
-	client.publish("/fan/status", payload=status, retain=True)
+	client.publish("/{0}/status".format(devid), payload=status, retain=True)
 
 client = mqtt.Client()
 client.on_connect = on_connect
