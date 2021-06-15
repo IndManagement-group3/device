a869fdfb7c0c65a8bb2b9a3e5af7fe714d7f905d
Kirs67 <kirilel200267@gmail.com>
2021-06-15 13:49:55 +0100

    The final version for fan

--- client.py
+++ client.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
+import serial
 import time
 
-pwmpin = 12
-pwmfreq = 1000
 timeout = 1 #in seconds
 
-devid = "fan"
+devid = "led0"
 
 turned = 0
 power = 0
 keepalivesync = 0
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD) #pins are numbered according to their physical order on the header
-GPIO.setup(pwmpin, GPIO.OUT)
-pwm = GPIO.PWM(pwmpin, pwmfreq)
-pwm.start(0)
+ser = serial.Serial ("/dev/ttyACM0", 9600)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -57,10 +51,17 @@ def on_message(client, userdata, msg):
 
 	update_status(client)
 
+def update_fan():
+	global turned, power
+	
+	command = bytes([turned*power])
+	print(command)
+	ser.write(command)
+
 def update_status(client):
 	global turned, power
 
-	pwm.ChangeDutyCycle(power * turned)
+	update_fan()
 
 	status = "{0};{1}".format(turned, power)
 	print(status)
@@ -82,6 +83,7 @@ while True:
 		turned = 0
 		update_status(client)
 
+	update_fan()
 	#print((time.clock_gettime(time.CLOCK_REALTIME) - keepalivesync))
 
 
